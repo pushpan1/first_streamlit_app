0944a33d577677c236da9e4d2af61c68040fe485
pn <122298985+pushpan1@users.noreply.github.com>
2023-01-09 12:01:00 -0500

    Update streamlit_app.py
    Added streamlit code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
-
+import streamlit
+streamlit.Title('My parents New Diner')
+streamlit.header('Break Fast Menu')
+streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Kale , Spinach and Oatmeal Smoothie')
+streamlit.text('Hard Boiled Free Range Egg')
